a46444e66663ab1a8963545aefa9d916f7bf54ff
Aldo <aldo.onelabs@gmail.com>
2022-01-03 16:35:32 +0700

    Initial project setup

---
+++ NeonDemoLibExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeonDemoLibExamples
+//
+//  Created by ALDO LAZUARDI on 03/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeonDemoLibExamples/NeonDemoLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonDemoLibExamplesApp.swift
+//  NeonDemoLibExamples
+//
+//  Created by ALDO LAZUARDI on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct NeonDemoLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
