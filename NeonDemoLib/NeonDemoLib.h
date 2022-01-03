2a89d6ecc45146c19f1eb8c428bf8ff78d9b040e
Aldo <aldo.onelabs@gmail.com>
2022-01-03 16:06:31 +0700

    Initial Commit

---
+++ NeonDemoLib/NeonDemoLib.h
@@ -0,0 +1,18 @@
+//
+//  NeonDemoLib.h
+//  NeonDemoLib
+//
+//  Created by ALDO LAZUARDI on 03/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NeonDemoLib.
+FOUNDATION_EXPORT double NeonDemoLibVersionNumber;
+
+//! Project version string for NeonDemoLib.
+FOUNDATION_EXPORT const unsigned char NeonDemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeonDemoLib/PublicHeader.h>
+
+
